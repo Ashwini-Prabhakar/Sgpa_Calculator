ed2c6940587b28a25e83faf65816dae9b5c67341
Ashwini-Prabhakar <130441714+Ashwini-Prabhakar@users.noreply.github.com>
2024-09-10 15:15:59 +0530

    Update Sgpa.java

--- SCalculator/src/main/java/canara/args/Sgpa.java
+++ SCalculator/src/main/java/canara/args/Sgpa.java
@@ -1,5 +1,6 @@
 package canara.args;
 
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -18,25 +19,117 @@ public class Sgpa extends HttpServlet{
 			System.out.println("Running doPost() method");
 			
 			String num = req.getParameter("number");
-			String m1 = req.getParameter("marks1");
-			String m2 = req.getParameter("marks2");
-			String c1=req.getParameter("credit1");
-			String c2=req.getParameter("credit2");
-			PrintWriter out=resp.getWriter();
+			String mark1="0";
+			String mark2="0";
+			String mark3="0";
+			String mark4="0";
+			String mark5="0";
+			String mark6="0";
+			String mark7="0";
+			String mark8="0";
+			String mark9="0";   
 			int n=Integer.valueOf(num);
-			int p1=Integer.valueOf(m1)*Integer.valueOf(c1);
-			int p2=Integer.valueOf(m2)*Integer.valueOf(c2);
-			int s=p1+p2;
-			int sc=Integer.valueOf(c1)+Integer.valueOf(c2);
-			double d=s/sc;
+			String mark[]=new String[n];
+			String cred[]=new String[n];
+			int p1[]=new int[n];
+			int mc[]=new int[n];
+			/*String cred[]=new String[n];
 			for(int i=0;i<n;i++) {
-				
+				mark[i]="0";
 			}
-			out.println(m1+" "+ m2+" ");
-			out.println(c1+" "+ c2+" ");
-			out.println(p1+" "+ p2+" ");
-			out.println(s+" "+ sc+" ");
-			out.println("The sgpa is "+d);
+			for(int i=0;i<n;i++) {
+				mark[i]=req.getParameter("mi");
+			}	*/
+			
+			mark1 = req.getParameter("m1");
+			mark2 = req.getParameter("m2");
+			mark3 = req.getParameter("m3");
+			mark4 = req.getParameter("m4");
+			mark5 = req.getParameter("m5");
+			mark6 = req.getParameter("m6");
+			mark7 = req.getParameter("m7");
+			mark8 = req.getParameter("m8");
+			  
+			
+			
+			mark[0]=mark1;
+			mark[1]=mark2;
+			mark[2]=mark3;
+			mark[3]=mark4;
+			mark[4]=mark5;
+			mark[5]=mark6;
+			mark[6]=mark7;
+			mark[7]=mark8;
+			
+			for(int i=0;i<n;i++) {
+				if(Integer.valueOf(mark[i])>=60 && Integer.valueOf(mark[i])<70) 
+					mc[i]=7;
+				
+				else if(Integer.valueOf(mark[i])>=70 && Integer.valueOf(mark[i])<80) 
+					mc[i]=8;
+				else if(Integer.valueOf(mark[i])>=80 && Integer.valueOf(mark[i])<90) 
+					mc[i]=9;
+				else if(Integer.valueOf(mark[i])>=90 && Integer.valueOf(mark[i])<=100) 
+					mc[i]=10;
+				else
+					mc[i]=0;
+			}
+			
+			String cred1="0";
+			String cred2="0";
+			String cred3="0";
+			String cred4="0";
+			String cred5="0";
+			String cred6="0";
+			String cred7="0";
+			String cred8="0";
+			
+			
+			cred1=req.getParameter("c1");
+			cred2=req.getParameter("c2");  
+			cred3=req.getParameter("c3");
+			cred4=req.getParameter("c4");
+			cred5=req.getParameter("c5");
+			cred6=req.getParameter("c6");
+			cred7=req.getParameter("c7");
+			cred8=req.getParameter("c8");
+			
+			
+			cred[0]=cred1;
+			cred[1]=cred2;
+			cred[2]=cred3;
+			cred[3]=cred4;
+			cred[4]=cred5;
+			cred[5]=cred6;
+			cred[6]=cred7;
+			cred[7]=cred8;
+			
+			
+			PrintWriter out=resp.getWriter();
+			int j=0;
+			while(j<n) {
+				p1[j]=mc[j]*Integer.valueOf(cred[j]);
+				j++;
+			}
+			int creditsum=0;
+			int prodsum=0;
+			for(int i=0;i<n;i++) {
+				creditsum+=Integer.valueOf(cred[i]);
+				prodsum+=p1[i];
+					
+			}
+			
+			double d=(double)prodsum/creditsum;  
+		/*	for(int i=0;i<n;i++) {
+				cred[i]="0";
+			}
+			for(int i=0;i<n;i++) {
+				cred[i]=req.getParameter("ce".replace('e',i));
+			}  */
+			
+			
+			out.println("The sum of credits is: "+creditsum+" The product sum is:  "+prodsum+" ");
+			out.println("The sgpa is "+String.format("%.2f",d));
 			
 			
 			
@@ -48,3 +141,4 @@ public class Sgpa extends HttpServlet{
 		
 
 }
+
